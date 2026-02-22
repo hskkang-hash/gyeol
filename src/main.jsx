4d89a6e3f4e575edaa148ceba4b3f3ec12bc487c
hskkang <hskkang@gaion.kr>
2026-02-22 13:05:54 +0000

    fix: Prevent server-side rendering of ReactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import './index.css';
 import './i18n'; // Import the i18n configuration
 import ThemeProvider from './contexts/ThemeProvider';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+if (typeof document !== 'undefined') {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>,
+  );
+}
