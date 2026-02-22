90d054001358953e4c205b17b1a4320c298d66fe
hskkang <hskkang@gaion.kr>
2026-02-22 16:54:56 +0000

    chore: Trigger new Cloudflare deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+// Triggering new deployment
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -43,4 +44,3 @@ function App() {
 }
 
 export default App;
-
